648754c7bc0b115dfaef14d22fe6aa0eaabcdc97
Oleksandr Nemesh <prevter@gmail.com>
2024-08-05 23:12:59 +0300

    fix include paths

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -1,4 +1,8 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://prevter.github.io/bindings-meta',
+    base: '/bindings-meta',
+    trailingSlash: 'always',
+});
